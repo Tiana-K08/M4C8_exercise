4393174f517fad50ba45f0e665d3918372df98de
“Tiana-K08” <tetkononenko2020@gmail.com>
2025-05-22 17:43:49 +0200

    initial commit

---
+++ script.js
@@ -0,0 +1,5 @@
+// Cree un bucle for en JS que imprima cada nombre en esta lista. miLista = “velma”, “exploradora”, “jane”, “john”, “harry”
+
+// Cree un bucle while que recorra la misma lista y también imprima los nombres. Nota: Recuerda crear un contador para que el ciclo no sea infinito.
+
+// Cree una función de flecha que devuelva "Hola mundo".
